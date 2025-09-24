7f40fbbf7288b8fb5916bb1bd915252e5fe1173a
larmarjago <112905079+larmarjago@users.noreply.github.com>
2025-09-24 23:15:54 +0100

    Updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,32 @@
-import React from "react";
-import Nav from "../Navigations/Nav";
-import Categories from "./Categories";
-import Footer from "../Navigations/Footer";
-import Newsletter from "../Navigations/NewsLetter";
-import Hero from "../Navigations/Hero";
+import React from 'react'
+import Nav from '../Navigations/Nav'
+import Categories from './Categories'
+import Footer from '../Navigations/Footer'
+import Newsletter from '../Navigations/NewsLetter'
+import Hero from '../Navigations/Hero'
 
 const Home = () => {
   return (
     <div>
       <div>
-        <Nav />
-      </div>
-      <div>
-        <Hero />
-      </div>
-      <div className="mt-20">
+              <Nav />
+              
+          </div>
+          <div>
+            <Hero />
+           </div>
+          <div className='mt-20'>
         <Categories />
-      </div>
-      <div>
-        <Newsletter />
-      </div>
-      <div>
-        <Footer />
-      </div>
+          </div>
+          <div>
+            <Newsletter/>
+          </div>
+          <div>
+             <Footer /> 
+          </div>
+
     </div>
   );
-};
+}
 
-export default Home;
+export default Home
