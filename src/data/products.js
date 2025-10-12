a2812edff58d8db28c67700662f74aab24c68aab
larmarjago <112905079+larmarjago@users.noreply.github.com>
2025-10-12 15:48:21 +0100

    updated ca

--- src/data/products.js
+++ src/data/products.js
@@ -1,130 +1,132 @@
-import hoodie1 from "../assets/hoodie1.jpg";
-import hoodie2 from "../assets/hoodie2.jpg";
+// import hoodie1 from "../assets/hoodie1.jpg";
+// import hoodie2 from "../assets/hoodie2.jpg";
 
-import hoodie4 from "../assets/hoodie4.jpg";
-
-
-import hoodie8 from "../assets/hoodie8.jpg";
-import jacket1 from "../assets/jacket1.jpg";
-
-import jacket4 from "../assets/jacket4.jpg";
-import tshirt1 from "../assets/tshirt1.jpg";
-import tshirt2 from "../assets/tshirt2.jpg";
-import tshirt3 from "../assets/tshirt3.jpg";
-import tshirt4 from "../assets/tshirt4.jpg";
-import tshirt5 from "../assets/tshirt5.jpg";
-import tshirt6 from "../assets/tshirt6.jpg";
-
-import pants1 from "../assets/pants1.jpg";
-import pants3 from "../assets/pants3.jpg";
-
-const products = [
-  // Hoodies (10 products)
-  {
-    id: 1,
-    title: "Black Street Wear Hoodie",
-    category: "hoodies",
-    price: 12000,
-    img: hoodie1,
-  },
-  {
-    id: 2,
-    title: "Navy Blue Hoodie",
-    category: "hoodies",
-    price: 12500,
-    img: hoodie2,
-  },
-  {
-    id: 3,
-    title: "Black Street Wear Hoodie",
-    category: "hoodies",
-    price: 11500,
-    img: hoodie4,
-  },
-  {
-    id: 4,
-    title: "Purple Street Wear Hoodie",
-    category: "hoodies",
-    price: 13000,
-    img: hoodie8,
-  },
-  {
-    id: 5,
-    title: "Olive Green Hoodie",
-    category: "hoodies",
-    price: 11800,
-    img: jacket1,
-  },
-  {
-    id: 6,
-    title: "Heather Grey Hoodie",
-    category: "hoodies",
-    price: 11000,
-    img: jacket4,
-  },
+// import hoodie4 from "../assets/hoodie4.jpg";
 
 
-  // T-Shirts (10 products)
-  {
-    id: 7,
-    title: "Classic White Tee",
-    category: "tshirts",
-    price: 6000,
-    img: tshirt1,
-  },
-  {
-    id: 8,
-    title: "Black Cotton Tee",
-    category: "tshirts",
-    price: 6500,
-    img: tshirt2,
-  },
-  {
-    id: 9,
-    title: "Navy Blue Tee",
-    category: "tshirts",
-    price: 6200,
-    img: tshirt3,
-  },
-  {
-    id: 10,
-    title: "Grey V-Neck Tee",
-    category: "tshirts",
-    price: 5800,
-    img: tshirt4,
-  },
-  {
-    id: 11,
-    title: "Red Graphic Tee",
-    category: "tshirts",
-    price: 7500,
-    img: tshirt5,
-  },
-  {
-    id: 12,
-    title: "Olive Green Tee",
-    category: "tshirts",
-    price: 6300,
-    img: tshirt6,
-  },
+// import hoodie8 from "../assets/hoodie8.jpg";
+// import jacket1 from "../assets/jacket1.jpg";
+
+// import jacket4 from "../assets/jacket4.jpg";
+// import tshirt1 from "../assets/tshirt1.jpg";
+// import tshirt2 from "../assets/tshirt2.jpg";
+// import tshirt3 from "../assets/tshirt3.jpg";
+// import tshirt4 from "../assets/tshirt4.jpg";
+// import tshirt5 from "../assets/tshirt5.jpg";
+// import tshirt6 from "../assets/tshirt6.jpg";
+
+// import pants1 from "../assets/pants1.jpg";
+// import pants3 from "../assets/pants3.jpg";
+
+// const products = [
+//   // Hoodies (10 products)
+//   {
+//     id: 1,
+//     title: "Black Street Wear Hoodie",
+//     category: "hoodies",
+//     price: 12000,
+//     img: hoodie1,
+//   },
+//   {
+//     id: 2,
+//     title: "Navy Blue Hoodie",
+//     category: "hoodies",
+//     price: 12500,
+//     img: hoodie2,
+//   },
+//   {
+//     id: 3,
+//     title: "Black Street Wear Hoodie",
+//     category: "hoodies",
+//     price: 11500,
+//     img: hoodie4,
+//   },
+//   {
+//     id: 4,
+//     title: "Purple Street Wear Hoodie",
+//     category: "hoodies",
+//     price: 13000,
+//     img: hoodie8,
+//   },
+//   {
+//     id: 5,
+//     title: "Olive Green Hoodie",
+//     category: "hoodies",
+//     price: 11800,
+//     img: jacket1,
+//   },
+//   {
+//     id: 6,
+//     title: "Heather Grey Hoodie",
+//     category: "hoodies",
+//     price: 11000,
+//     img: jacket4,
+//   },
+
+
+//   // T-Shirts (10 products)
+//   {
+//     id: 7,
+//     title: "Classic White Tee",
+//     category: "tshirts",
+//     price: 6000,
+//     img: tshirt1,
+//   },
+//   {
+//     id: 8,
+//     title: "Black Cotton Tee",
+//     category: "tshirts",
+//     price: 6500,
+//     img: tshirt2,
+//   },
+//   {
+//     id: 9,
+//     title: "Navy Blue Tee",
+//     category: "tshirts",
+//     price: 6200,
+//     img: tshirt3,
+//   },
+//   {
+//     id: 10,
+//     title: "Grey V-Neck Tee",
+//     category: "tshirts",
+//     price: 5800,
+//     img: tshirt4,
+//   },
+//   {
+//     id: 11,
+//     title: "Red Graphic Tee",
+//     category: "tshirts",
+//     price: 7500,
+//     img: tshirt5,
+//   },
+//   {
+//     id: 12,
+//     title: "Olive Green Tee",
+//     category: "tshirts",
+//     price: 6300,
+//     img: tshirt6,
+//   },
   
 
-  // Bottoms (10 products)
-  {
-    id: 13,
-    title: "Classic Denim Jeans",
-    category: "bottoms",
-    price: 15000,
-    img: pants1,
-  },
-  {
-    id: 14,
-    title: "Black Slim Jeans",
-    category: "bottoms",
-    price: 16500,
-    img: pants3,
-  },
+//   // Bottoms (10 products)
+//   {
+//     id: 13,
+//     title: "Classic Denim Jeans",
+//     category: "bottoms",
+//     price: 15000,
+//     img: pants1,
+//   },
+//   {
+//     id: 14,
+//     title: "Black Slim Jeans",
+//     category: "bottoms",
+//     price: 16500,
+//     img: pants3,
+//   },
 
 
-];
+// ];
+
+const products = []
 export default products;
